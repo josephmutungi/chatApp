5ea6d7d92e9e969263614501895dfe4afa4fd930
josephmutungi <josehstar1@gmail.com>
2026-08-18 20:15:23 +0300

    Fixed 500 login error

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, //process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7days
     });
