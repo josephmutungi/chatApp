e22be70d18d423cb66ec5b452698e42726e90aeb
josephmutungi <josehstar1@gmail.com>
2026-08-19 17:08:13 +0300

    Updated login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,7 +40,7 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    const { email, password } = await req.body;
+    const { email, password } = req.body;
 
     if (!email || !password) {
       return res
@@ -65,13 +65,13 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7days
     });
 
     return res.status(200).json({ message: "Login successful" });
   } catch (error) {
-    console.error(error);
+    console.error("Login error: ", error);
     return res.status(500).json({ error: "login failed!" });
   }
 };
--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ const io = new Server(server, {
   },
 });
 
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
 
