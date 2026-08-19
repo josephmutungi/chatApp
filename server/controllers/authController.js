8a691eeff6968575e0033674915d6489f43e4182
josephmutungi <josehstar1@gmail.com>
2026-08-19 17:34:25 +0300

    Updated cookies setting

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -63,9 +63,6 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7days
     });
 
