ad7cddddd02b4902ebd6ec8c39988a82649ba24b
josephmutungi <josehstar1@gmail.com>
2026-08-19 17:23:24 +0300

    Updated the token setting in the login route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -64,8 +64,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: false,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7days
     });
 
